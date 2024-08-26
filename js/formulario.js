6cb49984bfde3628444ecc2f5db15ff1209c317d
Santiago Villaseñor <sanvilladuron@gmail.com>
2024-08-26 13:38:21 -0600

    primer commit

---
+++ js/formulario.js
@@ -0,0 +1,31 @@
+function jugarAdivinarNumero() {
+    const numeroAleatorio = 8;
+
+    let intentos = 0;
+    let adivinado = false;
+
+    while (adivinado === false) {
+
+        const intento = parseInt(prompt("Intenta adivinar que número estoy pensando del 1 al 10"));
+        intentos++;
+        
+        if (intento === numeroAleatorio) {
+            alert("¡Si, me ganaste en solo " + intentos + " intentos!");
+            adivinado = true;
+
+            let juguemos = prompt("¿Jugamos?")
+            if (juguemos === true) {
+                jugarAdivinarNumero();
+            } else {
+                alert ("Gracias por jugar conmigo");
+            }
+
+        } else if (intento < numeroAleatorio) {
+            alert("Fallaste mi chavo, es mas, inténtalo otra vez.");
+        } else if ("intento < numeroAleatorio") {
+            alert("Te pasaste, es menos, siguelo intentando.");
+        } else {
+            alert("Eso no es válido, intenta con números por favor.")
+        }
+    }   
+}
